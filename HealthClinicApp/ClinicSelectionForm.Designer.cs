e95f2d39c33b82725204b997a0519c2f5e52e914
GoanChap <stalliondenis@gmail.com>
2023-05-29 16:10:50 +1200

    Add project files.

---
+++ HealthClinicApp/ClinicSelectionForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace HealthClinicApp
+{
+    partial class ClinicSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clinicSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.docSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.clinicRatingLabel = new System.Windows.Forms.Label();
+            this.clinicContactNoLabel = new System.Windows.Forms.Label();
+            this.clinicAddressLabel = new System.Windows.Forms.Label();
+            this.clinicNameLabel = new System.Windows.Forms.Label();
+            this.docContactLabel = new System.Windows.Forms.Label();
+            this.docSpecialityLabel = new System.Windows.Forms.Label();
+            this.docNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clinicSelectionComboBox
+            // 
+            this.clinicSelectionComboBox.FormattingEnabled = true;
+            this.clinicSelectionComboBox.Location = new System.Drawing.Point(121, 105);
+            this.clinicSelectionComboBox.Name = "clinicSelectionComboBox";
+            this.clinicSelectionComboBox.Size = new System.Drawing.Size(337, 33);
+            this.clinicSelectionComboBox.TabIndex = 0;
+            this.clinicSelectionComboBox.SelectedIndexChanged += new System.EventHandler(this.clinicSelectionComboBox_SelectedIndexChanged);
+            // 
+            // docSelectionComboBox
+            // 
+            this.docSelectionComboBox.FormattingEnabled = true;
+            this.docSelectionComboBox.Location = new System.Drawing.Point(674, 105);
+            this.docSelectionComboBox.Name = "docSelectionComboBox";
+            this.docSelectionComboBox.Size = new System.Drawing.Size(337, 33);
+            this.docSelectionComboBox.TabIndex = 1;
+            this.docSelectionComboBox.SelectedIndexChanged += new System.EventHandler(this.docSelectionComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a clinic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(680, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select a Doctor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Clinic Name: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 31);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Clinic Address: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 31);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Contact No.: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(145, 410);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 31);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Clinic Rating: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(680, 345);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(191, 31);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Doctor Contact: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(680, 279);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(211, 31);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Doctor Speciality: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(680, 204);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(171, 31);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Doctor Name: ";
+            // 
+            // clinicRatingLabel
+            // 
+            this.clinicRatingLabel.AutoSize = true;
+            this.clinicRatingLabel.Location = new System.Drawing.Point(284, 410);
+            this.clinicRatingLabel.Name = "clinicRatingLabel";
+            this.clinicRatingLabel.Size = new System.Drawing.Size(63, 31);
+            this.clinicRatingLabel.TabIndex = 15;
+            this.clinicRatingLabel.Text = "../10";
+            // 
+            // clinicContactNoLabel
+            // 
+            this.clinicContactNoLabel.AutoSize = true;
+            this.clinicContactNoLabel.Location = new System.Drawing.Point(284, 331);
+            this.clinicContactNoLabel.Name = "clinicContactNoLabel";
+            this.clinicContactNoLabel.Size = new System.Drawing.Size(34, 31);
+            this.clinicContactNoLabel.TabIndex = 14;
+            this.clinicContactNoLabel.Text = "...";
+            // 
+            // clinicAddressLabel
+            // 
+            this.clinicAddressLabel.AutoSize = true;
+            this.clinicAddressLabel.Location = new System.Drawing.Point(284, 265);
+            this.clinicAddressLabel.Name = "clinicAddressLabel";
+            this.clinicAddressLabel.Size = new System.Drawing.Size(34, 31);
+            this.clinicAddressLabel.TabIndex = 13;
+            this.clinicAddressLabel.Text = "...";
+            // 
+            // clinicNameLabel
+            // 
+            this.clinicNameLabel.AutoSize = true;
+            this.clinicNameLabel.Location = new System.Drawing.Point(284, 190);
+            this.clinicNameLabel.Name = "clinicNameLabel";
+            this.clinicNameLabel.Size = new System.Drawing.Size(34, 31);
+            this.clinicNameLabel.TabIndex = 12;
+            this.clinicNameLabel.Text = "...";
+            // 
+            // docContactLabel
+            // 
+            this.docContactLabel.AutoSize = true;
+            this.docContactLabel.Location = new System.Drawing.Point(879, 345);
+            this.docContactLabel.Name = "docContactLabel";
+            this.docContactLabel.Size = new System.Drawing.Size(34, 31);
+            this.docContactLabel.TabIndex = 18;
+            this.docContactLabel.Text = "...";
+            // 
+            // docSpecialityLabel
+            // 
+            this.docSpecialityLabel.AutoSize = true;
+            this.docSpecialityLabel.Location = new System.Drawing.Point(879, 279);
+            this.docSpecialityLabel.Name = "docSpecialityLabel";
+            this.docSpecialityLabel.Size = new System.Drawing.Size(34, 31);
+            this.docSpecialityLabel.TabIndex = 17;
+            this.docSpecialityLabel.Text = "...";
+            // 
+            // docNameLabel
+            // 
+            this.docNameLabel.AutoSize = true;
+            this.docNameLabel.Location = new System.Drawing.Point(879, 204);
+            this.docNameLabel.Name = "docNameLabel";
+            this.docNameLabel.Size = new System.Drawing.Size(34, 31);
+            this.docNameLabel.TabIndex = 16;
+            this.docNameLabel.Text = "...";
+            // 
+            // ClinicSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 664);
+            this.Controls.Add(this.docContactLabel);
+            this.Controls.Add(this.docSpecialityLabel);
+            this.Controls.Add(this.docNameLabel);
+            this.Controls.Add(this.clinicRatingLabel);
+            this.Controls.Add(this.clinicContactNoLabel);
+            this.Controls.Add(this.clinicAddressLabel);
+            this.Controls.Add(this.clinicNameLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.docSelectionComboBox);
+            this.Controls.Add(this.clinicSelectionComboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ClinicSelectionForm";
+            this.Text = "Healthcare Selection";
+            this.Load += new System.EventHandler(this.ClinicSelectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox clinicSelectionComboBox;
+        private System.Windows.Forms.ComboBox docSelectionComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label clinicRatingLabel;
+        private System.Windows.Forms.Label clinicContactNoLabel;
+        private System.Windows.Forms.Label clinicAddressLabel;
+        private System.Windows.Forms.Label clinicNameLabel;
+        private System.Windows.Forms.Label docContactLabel;
+        private System.Windows.Forms.Label docSpecialityLabel;
+        private System.Windows.Forms.Label docNameLabel;
+    }
+}
+
---
+++ HealthClinicApp/ClinicSelectionForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthClinicApp
+{
+    public partial class ClinicSelectionForm : Form
+    {
+        DataTable dtClinics = new DataTable();
+        DataTable dtDocs = new DataTable();
+        DataTable dtSpecificClinicDoctors = new DataTable();
+        public ClinicSelectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClinicSelectionForm_Load(object sender, EventArgs e)
+        {
+            FillClinicTable();
+            FillDoctorTable();  
+
+            clinicSelectionComboBox.DataSource = dtClinics;
+            clinicSelectionComboBox.DisplayMember = "CName";
+
+                    
+
+        }
+
+        private void FillClinicTable()
+        {
+            dtClinics.Columns.Add("CID", typeof(int));
+            dtClinics.Columns.Add("CName");
+            dtClinics.Columns.Add("CAddress");
+            dtClinics.Columns.Add("CContact");
+            dtClinics.Columns.Add("CRating");
+
+            dtClinics.Rows.Add(1, "Dr. Parsnani Private Clinic", "Aga Khan Baug", "7858758756","8/10");
+            dtClinics.Rows.Add(2, "Dentomania", "Yari Road", "7857657476", "9/10");
+            dtClinics.Rows.Add(3, "Dr. Dhumaskar Hospital", "Goa", "080775454","7.5/10");
+            dtClinics.Rows.Add(4, "Auckland City Hospital", "Auckland", "4321447890","5/10");
+
+        }
+
+        private void clinicSelectionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clinicNameLabel.Text = dtClinics.Rows[clinicSelectionComboBox.SelectedIndex]["CName"].ToString();
+            clinicAddressLabel.Text = dtClinics.Rows[clinicSelectionComboBox.SelectedIndex]["CAddress"].ToString();
+            clinicRatingLabel.Text = dtClinics.Rows[clinicSelectionComboBox.SelectedIndex]["CRating"].ToString();
+            clinicContactNoLabel.Text = dtClinics.Rows[clinicSelectionComboBox.SelectedIndex]["CContact"].ToString();
+
+            dtSpecificClinicDoctors = dtDocs.Select("CID = " + dtClinics.Rows[clinicSelectionComboBox.SelectedIndex]["CID"]).CopyToDataTable();
+            docSelectionComboBox.DataSource = dtSpecificClinicDoctors;
+            docSelectionComboBox.DisplayMember = "DName";
+
+        }
+
+        private void docSelectionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            docNameLabel.Text = dtSpecificClinicDoctors.Rows[docSelectionComboBox.SelectedIndex]["DName"].ToString();
+            docSpecialityLabel.Text = dtSpecificClinicDoctors.Rows[docSelectionComboBox.SelectedIndex]["DSpeciality"].ToString();
+            docContactLabel.Text = dtSpecificClinicDoctors.Rows[docSelectionComboBox.SelectedIndex]["DContact"].ToString();
+
+        }
+
+        private void FillDoctorTable()
+        {
+            dtDocs.Columns.Add("CID", typeof(int));
+            dtDocs.Columns.Add("DName");
+            dtDocs.Columns.Add("DSpeciality");
+            dtDocs.Columns.Add("DContact");
+
+            dtDocs.Rows.Add(1, "Dr. Parsnani", "GP", "7858758756");
+            dtDocs.Rows.Add(2, "Dr. Rehmatula", "Pediatrician", "7857657476");
+            dtDocs.Rows.Add(2, "Dr. Ai Hoshino", "Obstetrics and gynaecology", "7855647476");
+            dtDocs.Rows.Add(2, "Dr. Gorou Amemiya", "Surgeon", "7857641512");
+            dtDocs.Rows.Add(3, "Dr. Apay Balachandran", "Dermatologist", "080775454");
+            dtDocs.Rows.Add(3, "Dr. Grisha Yeager", "Oncologist", "0845674");
+            dtDocs.Rows.Add(3, "Dr. Dhumaskar", "GP", "080780867");
+            dtDocs.Rows.Add(4, "Dr. Velita Menezes", "Anesthesiologist", "4321423454");
+            dtDocs.Rows.Add(4, "Dr. Sakura Yamauchi", "Cardiologist", "4321454657");
+            dtDocs.Rows.Add(4, "Dr. Gojo Saturou", "Psychiatrist", "43214457468");
+            dtDocs.Rows.Add(4, "Dr. Ainz Oul Gowl", "Pathologist", "4321445748");
+
+        }
+    }
+}
